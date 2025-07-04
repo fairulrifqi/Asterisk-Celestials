efc51c0b202aa125f19983e99b903c0e44df3790
fairulrifqi <fairulrifqi962@gmail.com>
2025-07-04 18:06:29 +0700

    Update deployment.py

--- Stage 4/deployment.py
+++ Stage 4/deployment.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import statistics as stats
-import seaborn
+import seaborn as sns
 import matplotlib.pyplot as plt
 import scipy.stats as sx
 import math
